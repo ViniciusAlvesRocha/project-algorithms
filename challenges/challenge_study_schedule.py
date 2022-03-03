1aeb55add6967193e8914b4eeca6966206ee9c0b
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2022-03-03 13:03:11 -0300

    Requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,2 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """ Faça o código aqui. """
+    cont = 0
+    for entry, exit in permanence_period:
+        try:
+            if entry <= target_time <= exit:
+                cont += 1
+        except TypeError:
+            return None
+
+    return cont
+
+# permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5), (4, 5)]
+# permanence_period = [(2, None), (1, 2)]
+# target_time = 1
