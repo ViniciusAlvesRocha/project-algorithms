a4a536c22cd5d1a917a7e5f157657af3312b9ad2
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2022-03-03 14:07:26 -0300

    remove line of test file challenges/challenge_palindromes_recursive

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -4,23 +4,3 @@ def is_palindrome_recursive(word, low_index, high_index):
     if low_index < high_index:
         return is_palindrome_recursive(word, low_index + 1, high_index - 1)
     return True
-
-word = "ANA"
-print(is_palindrome_recursive(word, 0, len(word) - 1))
-# saída: True
-
-word = "SOCOS"
-print(is_palindrome_recursive(word, 0, len(word) - 1))
-# saída: True
-
-word = "REVIVER"
-print(is_palindrome_recursive(word, 0, len(word) - 1))
-# saída: True
-
-word = "COXINHA"
-print(is_palindrome_recursive(word, 0, len(word) - 1))
-# saída: False
-
-word = "AGUA"
-print(is_palindrome_recursive(word, 0, len(word) - 1))
-# saída: False
