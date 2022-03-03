6e66c2c380a8aa481dad665af4cda98891f910e6
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2022-03-03 15:03:44 -0300

    Requirement 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,36 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    if len(first_string) != len(second_string):
+        return False
+    first_string = [ord(character) for character in first_string]
+    second_string = [ord(character) for character in second_string]
+    first_string = quicksort(first_string, 0, len(first_string) - 1)
+    second_string = quicksort(second_string, 0, len(second_string) - 1)
+    return first_string == second_string
+
+
+def quicksort(array, low, high):
+    if len(array) == 1:
+        return array
+
+    if low < high:
+        partition_index = partition(array, low, high)
+        quicksort(array, low, partition_index - 1)
+        quicksort(array, partition_index + 1, high)
+        return array
+
+def partition(array, low, high):
+    i = low - 1
+    pivot = array[high]  # pivot
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            array[i], array[j] = array[j], array[i]
+    array[i + 1], array[high] = array[high], array[i + 1]
+
+    return i + 1
+
+
+""" array = [100, 4, 6, 33, 56, 67]
+quicksort(array, 0, len(array) - 1)
+print(array) """
