ae4fda5c9227cfaea6656213b00fa0f16a5f228a
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2022-03-03 20:54:11 -0300

    Requirement 5

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,2 +1,11 @@
 def is_palindrome_iterative(word):
-    """ Faça o código aqui. """
+    if word == "":
+        return False
+    if len(word) == 1:
+        return True
+    high_index = len(word) - 1
+    for character in word:
+        if character != word[high_index]:
+            return False
+        high_index -= 1
+    return True
